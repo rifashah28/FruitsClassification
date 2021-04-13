8507ad81d9c8a73424f95a2a6040ec2ee77f7559
amaranakamura <43504715+amaranakamura@users.noreply.github.com>
2021-04-13 15:37:20 -0700

    Update load.py

--- loading data/load.py
+++ loading data/load.py
@@ -3,4 +3,17 @@ img_size = 224
 def get_data(data_dir):
   data = []
   for label in labels:
-    path = os.path.join(data_dir, label) ...
+    path = os.path.join(data_dir, label)
+    class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+  
+#to access training/testing data
+train = get_data('../input/traintestsports/Main/train')
+test = get_data('../input/traintestsports/Main/test')
